f0e8cb7d5600e2abdee3988158ce973aad42ad32
riosnicolass <nicolasrios91@hotmail.com>
2022-08-25 17:03:45 -0300

    feat: issue number 1

--- guess-pokemon/src/App.tsx
+++ guess-pokemon/src/App.tsx
@@ -1,5 +1,41 @@
-function App() {
-  return <main>Let&apos;s get this party started</main>;
-}
+import {useState, useEffect} from "react";
+
+import {Pokemon} from "./types";
+import api from "./api";
+import {theme} from "./utils";
+
+const App = () => {
+  const [pokemon, setPokemon] = useState<Pokemon | null>(null);
+  const [showPokemon, setShowPokemon] = useState(false);
+  const [searchValue, setSearchValue] = useState<string>("");
+
+  const handleOnClick = () => {
+    setShowPokemon(true);
+  };
+
+  useEffect(() => {
+    api.random().then((response) => setPokemon(response));
+  }, []);
+
+  return (
+    <main>
+      <p className={`${theme.tagType.text} ${theme.status.primary}`}>
+        {showPokemon ? pokemon?.name : <br />}
+      </p>
+      <img className={showPokemon ? "show-pokemon" : ""} src={pokemon?.image} />
+      <div style={{display: "flex", width: "400px"}}>
+        <input
+          className={theme.tagType.input}
+          id="guess-pokemon"
+          type="text"
+          onChange={(e) => setSearchValue(e.target.value)}
+        />
+        <button className={theme.tagType.button} onClick={() => handleOnClick()}>
+          Guess
+        </button>
+      </div>
+    </main>
+  );
+};
 
 export default App;
